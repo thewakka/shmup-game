7ad6b883159d6aae7ceeb9f747a83d5043c0eaa2
thewakka <61843659+thewakka@users.noreply.github.com>
2020-03-05 18:41:03 +0100

    Upload first state of project

---
+++ Assets/My Custom Assets/FollowPlayer.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var player : Transform;
+var offset : Vector3;
+
+function Update () {
+
+		transform.position = player.position + offset;
+}
---
+++ Assets/My Custom Assets/Player/PlayerMovement.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var rb : Rigidbody;
+
+var forwardForce = 2000f;
+var sidewayForce = 100f;
+
+// We marked this as "Fixed"Update because we
+// are using it to mess with physics.
+function FixedUpdate () {
+	
+	rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+	
+	if (Input.GetKey("d")) {
+		rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+	}
+	
+	if (Input.GetKey("a")) {
+		rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+	}
+}
